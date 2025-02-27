bda014454e59eabf5d72759a1f27e87f522ece1c
RahulB <rahulbisht1012@gmail.com>
2025-02-27 20:40:37 +0530

    Update

--- app.ts
+++ app.ts
@@ -21,7 +21,7 @@ app.use(cookieParser());
 // cors => cross origin resource sharing
 app.use(
 	cors({
-		origin: ["https://e-learning-frontend-flax.vercel.app/"],
+		origin: ["https://e-learning-frontend-flax.vercel.app"],
 		credentials: true,
 	})
 );
