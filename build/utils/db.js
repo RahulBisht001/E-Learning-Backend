f2a8f9a1c2bb9ddd599380749f1f9e2ecac39146
RahulB <rahulbisht1012@gmail.com>
2025-02-27 19:56:57 +0530

    Server Deployment

---
+++ build/utils/db.js
@@ -0,0 +1,21 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+require("dotenv").config();
+const dbUrl = process.env.DB_URL || "";
+const connectDB = async () => {
+    try {
+        await mongoose_1.default.connect(dbUrl).then((data) => {
+            console.log(`Database connected with ${data.connection.host}`);
+        });
+    }
+    catch (error) {
+        console.log(error.message);
+        console.log("Retrying to connect to the database in 5 seconds");
+        setTimeout(connectDB, 5000);
+    }
+};
+exports.default = connectDB;
